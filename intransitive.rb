3a480ef579f9d3417eee39583c89f04afa17d8ce
pattywitwhy <pattyxhwang@gmail.com>
2020-02-12 15:06:59 -0600

    has_many associations testing with books and authors

---
+++ intransitive.rb
@@ -0,0 +1,44 @@
+require 'sqlite3'
+require 'active_record'
+
+# Set up a database that resides in RAM
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: ':memory:'
+)
+
+# Set up database tables and columns
+ActiveRecord::Schema.define do
+  create_table :authors, force: true do |t|
+    t.string :name
+
+  end
+  create_table :books, force: true do |t|
+    t.string :title
+    t.references :author
+  end
+end
+
+# Set up model classes
+class ApplicationRecord < ActiveRecord::Base
+  self.abstract_class = true
+end
+
+class Author < ApplicationRecord
+  has_many :books
+end
+
+class Book < ApplicationRecord
+  belongs_to :authors
+end
+
+
+
+# irb -r "./filename.rb"
+
+#OR
+
+#load ./filename.rb
+
+
+# https://blog.teamtreehouse.com/active-record-without-rails-app
